45a91feab960902407d28faf7276ab3f457fa6ce
ahmat <116895320+BigApple35@users.noreply.github.com>
2025-09-18 00:21:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-  origin: ["https://43432e968c95.ngrok-free.app", "https://siandu.vercel.app"],
+  origin: ["https://43432e968c95.ngrok-free.app", "https://siandu.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
