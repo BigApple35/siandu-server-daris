01bd01f91c6c775555e2a24f42e3627e7d218ff8
ahmat <116895320+BigApple35@users.noreply.github.com>
2025-09-17 21:43:06 +0700

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -23,6 +23,10 @@ export const authenticateAdminToken = (req, res, next) =>{
     if (!token) {
         return res.status(401).json({ message: 'Access Denied: No token provided' });
     }
+res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
 
     jwt.verify(token, secretKey, (err, user) => {
         if (err) {
